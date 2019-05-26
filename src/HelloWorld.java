978bb0b09b58e480ab6557217588c8577f172727
Ravi <ravi2krishna@gmail.com>
2019-05-26 12:12:05 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,9 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         
         int first = 10;
-        int second = 20;
+        int second = 80;
 
-        int sum = first * second;
+        int sum = first + second;
 
         System.out.println("The product is: " + sum);
     }
